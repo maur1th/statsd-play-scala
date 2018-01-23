0ee5ecabd659c46a7bf1bd3b9115ae5bbcabab5b
Nicolas Colomer <ncolomer@users.noreply.github.com>
2018-01-23 11:35:45 +0100

    User jmxtrans-agent

--- demo-app/app/Global.scala
+++ demo-app/app/Global.scala
@@ -1,12 +1,10 @@
 import com.codahale.metrics.JmxReporter
 import com.kenshoo.play.metrics.{MetricsRegistry, MetricsFilter}
-import org.jmxtrans.embedded.config.ConfigurationParser
 import play.api.{Application, GlobalSettings}
 import play.api.mvc.WithFilters
 
 object Global extends WithFilters(MetricsFilter) with GlobalSettings {
   override def onStart(app: Application) {
     JmxReporter.forRegistry(MetricsRegistry.defaultRegistry).build.start
-    new ConfigurationParser().newEmbeddedJmxTrans("classpath:jmxtrans.json").start
   }
 }
--- demo-app/build.sbt
+++ demo-app/build.sbt
@@ -9,7 +9,6 @@ scalaVersion := "2.11.6"
 libraryDependencies ++= Seq( jdbc , anorm , cache , ws )
 
 libraryDependencies ++= Seq(
-  "org.jmxtrans.embedded" % "embedded-jmxtrans" % "1.0.15+",
   "com.kenshoo" %% "metrics-play" % "2.3.0_0.1.9"
 )
 
